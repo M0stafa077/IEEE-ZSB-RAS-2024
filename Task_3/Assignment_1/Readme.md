87cb4edf97b59de1a50ee0afb7feb8f38b080d3b
Mostafa_Asaad <ma4190243@gmail.com>
2024-04-15 14:19:40 +0200

    Task_3

---
+++ Task_3/Assignment_1/Readme.md
@@ -0,0 +1,9 @@
+# Calculator in C language
+
+### Note
+
+    To run this project :
+        - Run a new terminal in the main directory.
+        - Type 'make'.
+        - Type '.\calculator.exe'.
+        - After trying the APP and exiting, just type 'make clean'. 
---
+++ Task_3/Assignment_1/operations/operations.h
@@ -0,0 +1,20 @@
+/**
+ * @file operations.h
+ * @author Mosta Asaad
+ * @brief A header file contains all the software 
+ * interfaces for the calculator app. 
+*/
+
+/* ----------- Includes ----------- */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* ----------- Types ----------- */
+typedef unsigned char uint8_t;
+
+/* ----------- Functions Declarations ----------- */
+void header_msg(void);
+void add(float, float);
+void sub(float, float);
+void mul(float, float);
+void divide(float, float);
